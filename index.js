7f887d0b87d66b37d4426bf26b5bce38a41de2f6
Gabriel Silveira <gabriel@gabrielsilveira.com.br>
2019-07-24 15:03:26 -0300

    Multilingue

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express')
 var nunjucks = require('nunjucks')
+const langs = require('./langs')
+const sigla = 'pt-br'
+const lang = langs[sigla]
 var app = express()
 
 // arquivos estáticos
@@ -11,8 +14,14 @@ nunjucks.configure('views', {
   express: app
 })
 
-app.get('/', (req, res) => res.render('home.njk'))
-app.get('/:page', (req, res) => res.render(`${req.params.page}.njk`))
+// home
+app.get('/', (req, res) => res.render('home.njk', lang.home))
+
+// outras páginas
+app.get('/:page', (req, res) => res.render(
+  `${req.params.page}.njk`,
+  lang[req.params.page]
+))
 
 const port = 21020
 app.listen(port, function () {
