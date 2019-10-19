49b911ee70d6228aeb677a310fcfd5533bc4acaf
Gabriel Silveira <gabriel@gabrielsilveira.com.br>
2019-10-19 19:38:38 -0300

    HTTPS redirect

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 new Vue({
-  delimiters: ['[[',']]',],
+  delimiters: ['[[',']]'],
   el: '#app',
   data: {
     URI: '/api/',
@@ -10,28 +10,28 @@ new Vue({
     targetPage: null,
   },
   methods: {
-    toggleMenu: function() {
+    toggleMenu: function () {
       let self = this;
       // exibe
       if(!this.openNav) self.mm();
 
-      setTimeout(function() {
+      setTimeout(function () {
         document.getElementById('hamburger-icon').classList.toggle('is-active');
         self.openNav = self.openNav ? false : true;
 
         // oculta
         if(!self.openNav) {
-          setTimeout(function() {
+          setTimeout(function () {
             self.mm(true);
           }, 750);
         }
       }, 50);
     },
-    closeMainMenu: function() {
+    closeMainMenu: function () {
       if(this.openNav)
         this.toggleMenu();
     },
-    pageTransition(remove = true, cb) {
+    pageTransition (remove = true, cb) {
       const div = document.getElementById('page-transition');
       if (remove) {
         div.classList.remove('full');
@@ -40,15 +40,15 @@ new Vue({
         setTimeout(cb, this.pageTransitionTime);
       }
     },
-    changeLocation() {
+    changeLocation () {
       window.location.href = this.targetPage;
     },
-    go: function(index) {
+    go: function (index) {
       this.closeMainMenu();
       this.targetPage = `./${this.sections[index].path}`;
       this.pageTransition(false, this.changeLocation);
     },
-    getGeneralText: async function(lang = 'pt-br') {
+    getGeneralText: async function (lang = 'pt-br') {
       const res = await axios.get(`${this.URI}texts/general/${lang}`);
       // textos do menu principal
       this.sections = res.data.menu;
@@ -56,29 +56,29 @@ new Vue({
       // textos gerais
       this.texts = res.data.info;
     },
-    getMenuItemText: function(index) {
+    getMenuItemText: function (index) {
       return this.sections[index].text;
     },
-    mm: function(hide) {
+    mm: function (hide) {
       document.getElementById('main-menu').style.display = hide ? 'none' : 'block';
     },
-    checkHttps: function() {
+    checkHttps: function () {
       if (location.protocol != 'https:' && location.hostname !== 'localhost') {
         const addr = window.location.href.substring(window.location.protocol.length);
-        location.href = `https: ${addr}`;
+        location.href = `https:${addr}`;
       }
     },
   },
-  beforeMount: function() {
+  beforeMount: function () {
     // verifica se está no HTTPS
     this.checkHttps();
   },
-  mounted: function() {
+  mounted: function () {
     // cria evento click fora do menu (para fechar)
     let mm = document.getElementById('main-menu');
     mm.addEventListener("click", this.closeMainMenu);
 
-    document.getElementById('nav-main-menu').addEventListener("click", function(e) {
+    document.getElementById('nav-main-menu').addEventListener("click", function (e) {
       e.stopPropagation();
     }, false);
 
